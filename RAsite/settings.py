c99b2de97b5ea1696311396ea685cef74844ea06
Chris Peabody <cgpzbd@mst.edu>
2017-06-12 10:31:29 -0500

    changed homepage text for demo

--- RAsite/settings.py
+++ RAsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pv+!z_k!_edjeim50k6_&gggs2s4&%9!cxk93513uccwr%5!fw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.106.23.59']
 
 
 # Application definition
